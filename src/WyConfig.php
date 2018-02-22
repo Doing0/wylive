a51ac21f70ba5594291dac1d1eabef52383b1d27
Adu <1576757995@qq.com>
2018-02-22 11:05:52 +0800

    初始化

---
+++ src/WyConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm
+ * PROJECT: 网易直播API
+ * User: Doing<vip.dulin@gmail.com>
+ * Date: 2018年2月22日
+ * Desc:网易直播配置文件
+ */
+//TODO: 修改这里配置信息;
+//AppKey
+const WY_APPKEY = '0c6a93c3d7ed11178623f51df299dd6c';
+//AppScecret
+const WY_APPSCECRET = '554f469e2cf7';
+//RequestType
+const WY_REQUESTTYPE = 'curl';
